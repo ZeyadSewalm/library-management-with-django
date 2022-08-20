2b3c5202b1a4fe1ae163d4e0be5ee99bd9888900
ZeyadSewalm <zeyadsewalm@gmail.com>
2022-08-20 18:27:29 +0200

    first commit

---
+++ lms/lms_app/forms.py
@@ -0,0 +1,31 @@
+from dataclasses import field, fields
+from pyexpat import model
+from tkinter import Widget
+from django import forms
+from .models import Book,Category
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name' : forms.TextInput(attrs={'class' : 'form-control'})
+        }
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title','author','photo_book','photo_author','pages','price','rental_price_day','rental_period','total_rental','status','category',]
+        widgets = {
+            'title' : forms.TextInput(attrs={'class' : 'form-control'}),
+            'author' : forms.TextInput(attrs={'class' : 'form-control'}),
+            'photo_book' : forms.FileInput(attrs={'class' : 'form-control'}),
+            'photo_author' : forms.FileInput(attrs={'class' : 'form-control'}),
+            'pages' : forms.NumberInput(attrs={'class' : 'form-control'}),
+            'price' : forms.NumberInput(attrs={'class' : 'form-control'}),
+            'rental_price_day' : forms.NumberInput(attrs={'class' : 'form-control','id':'rentalprice' }),
+            'rental_period' : forms.NumberInput(attrs={'class' : 'form-control', 'id':'rentaldays'}),
+            'total_rental' : forms.NumberInput(attrs={'class' : 'form-control', 'id':'totalrental'}),
+            'status' : forms.Select(attrs={'class' : 'form-control'}),
+            'category' : forms.Select(attrs={'class' : 'form-control'}),        
+        }
